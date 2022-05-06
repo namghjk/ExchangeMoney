9f71551485295a5207cbc8f7d9bb38cacd214628
namghjk <namghjk@namghjks-MacBook-Pro.local>
2022-05-06 23:25:44 +0700

    fix history

--- app/src/main/java/com/namghjk/exchangemoney/HistoryActivity.java
+++ app/src/main/java/com/namghjk/exchangemoney/HistoryActivity.java
@@ -2,8 +2,11 @@ package com.namghjk.exchangemoney;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -15,14 +18,21 @@ import Model.History;
 
 public class HistoryActivity extends AppCompatActivity {
 
+    ImageButton btnBack;
     private ListView lsvHistory;
     private HistoryAdapter adapterHistory;
     ArrayList<History> historyArrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(e -> {
+            finish();
+        });
 
         lsvHistory = findViewById(R.id.lsvHistory);
         historyArrayList = new ArrayList<>();
@@ -32,6 +42,8 @@ public class HistoryActivity extends AppCompatActivity {
         History history = (History) getIntent().getSerializableExtra("history");
         historyArrayList.add(history);
         Log.e("history",historyArrayList.toString());
+
+
 
 
 
